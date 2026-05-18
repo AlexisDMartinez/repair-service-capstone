5af0595cf759d1678fad659ea68b97d8559b73a8
Alexi <alexi@example.com>
2026-05-17 20:38:59 -0500

    Fix service API connection

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "repair-service-backend.onrender.com/api/services"
+  baseURL: "https://repair-service-backend.onrender.com/api"
 });
 
 API.interceptors.request.use((req) => {
