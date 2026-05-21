1d3a394f7fbb00a8d2023b6c8e5db3da2615dbe9
Alexi <alexi@example.com>
2026-05-21 18:08:54 -0500

    Add logout functionality

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,14 +1,24 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Navbar() {
 
   const token = localStorage.getItem("token");
+
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+
+    navigate("/login");
+  };
 
   return (
     <nav className="navbar">
       <h2 className="logo">A&S Industrial</h2>
 
       <div className="nav-links">
+
         <Link to="/">Home</Link>
 
         <Link to="/services">Services</Link>
@@ -21,11 +31,23 @@ function Navbar() {
           <Link to="/dashboard">Dashboard</Link>
         )}
 
-        <Link to="/login">Account</Link>
+        {!token ? (
+          <Link to="/login">Account</Link>
+        ) : (
+          <button
+            className="logout-button"
+            onClick={handleLogout}
+          >
+            Logout
+          </button>
+        )}
+
       </div>
     </nav>
   );
 }
 
 export default Navbar;
+
+
 
